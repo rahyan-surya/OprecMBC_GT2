4bc7ccb90e519faf68b4d7197404701e8a887b80
FirzaYudistira24 <firza.yudistira24@gmail.com>
2022-11-06 20:05:13 +0700

    Mengerjakan nomor 3

--- Tugas 2_Python/Nomor 3.py
+++ Tugas 2_Python/Nomor 3.py
@@ -1,1 +1,38 @@
-
+def main():
+    import os
+    import time
+    print ("="*31)
+    print("JADWAL SERAGAM TELKOM UNIVERITY")
+    print ("="*31)
+    hari = input("Masukkan hari : ")
+    if hari == "senin":
+        print("Seragam hari senin adalah Kemeja Merah Telkom")
+        time.sleep(5)
+        os.system('cls')
+        main()
+    elif hari == "selasa":
+        print("Seragam hari selasa adalah Kemeja Putih Telkom")
+        time.sleep(5)
+        os.system('cls')
+        main()
+    elif hari == "rabu":
+        print("Seragam hari rabu adalah Kemeja putih Telkom")
+        time.sleep(5)
+        os.system('cls')
+        main()
+    elif hari == "kamis":
+        print("Seragam hari kamis adalah Kemeja kotak-kotak atau kemeja bebas")
+        time.sleep(5)
+        os.system('cls')
+        main()
+    elif hari == "jumat":
+        print("Seragam hari jumat adalah Kemeja batik atau kemeja bebas")
+        time.sleep(5)
+        os.system('cls')
+        main()
+    else:
+        print("Data tidak ditemukan, silahkan masukkan lagi")
+        time.sleep(3)
+        os.system('cls')
+        main()
+main()
