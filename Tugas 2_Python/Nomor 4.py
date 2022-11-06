674f22da85f15a871152bcbc0d23f64d83ddc4a6
unknown <rahyansurya8j@gmail.com>
2022-11-06 23:24:04 +0700

    Mengerjakan Nomor 4

--- Tugas 2_Python/Nomor 4.py
+++ Tugas 2_Python/Nomor 4.py
@@ -1,1 +1,12 @@
-
+#membuat pola n x n dari * dan #
+n = int(input("Masukkan n :"))
+baris = n
+kolom = n
+print("Pola N x N")
+for i in range(1, baris+1):
+    for j in range(1, kolom+1):
+        if(i == 1 or i == baris or j == 1 or j == kolom):
+            print("*", end = '')
+        else:
+            print("#", end = '')
+    print()
